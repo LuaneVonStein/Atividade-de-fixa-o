b21be13c72506583cc3ac67a3ba01528077554a6
Luiz F. Picolo <luizpicolo@gmail.com>
2023-07-05 17:21:34 -0400

    Exercícios

---
+++ Script.js
@@ -0,0 +1,48 @@
+//Exercicio 4//
+let valor = 10
+  
+if(valor%2 === 0)  {
+  console.log("É par");
+} else {
+console.log("É impar");
+}
+
+//Exercicio 5//
+let numero = 5;
+
+if(numero > 0) { //quando é verdadeiro//
+console.log("É verdadeiro");
+} else {//quando é falso//
+ console.log("É falso");
+}
+
+//Exercicio 9 //
+let contador = 1;
+
+ while(contador <= 10){
+    console.log(contador);
+    let valor = 10
+if(valor%2 === 0)  {
+  console.log("Apenas numeros pares");
+} else {
+console.log(".");
+}
+    contador++;
+}
+
+
+//Exercicio 18 //
+let num4;
+
+do{
+    num4 = parseInt(prompt("Digite dois numero "));
+}while (num4 < 1 || num4 > 100);
+console.log("Você digitou o numero " + num4);
+
+if(valor%2 === 0)  {
+  console.log("É par");
+} else {
+console.log("É impar");
+}
+
+//Exercicio 16 //
